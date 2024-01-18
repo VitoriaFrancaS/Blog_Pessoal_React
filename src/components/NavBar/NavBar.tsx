098bb8ed533d128b794582534d93c14d5187550e
Vitoria F <vitoria.s.franca@gmail.com>
2024-01-18 14:00:10 -0300

    Conexão com o API cadastro e login

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,5 +1,6 @@
 import { Link, useNavigate } from 'react-router-dom'
 
+
 function Navbar() {
  
   
@@ -11,7 +12,6 @@ function Navbar() {
             <div className='text-2xl font-bold uppercase'>Blog Pessoal</div>
 
             <div className='flex gap-4'>
-              <Link to='/login' className='hover:underline'>Login</Link>
               <Link to='/home' className='hover:underline'>Home</Link>
               <div className='hover:underline'>Postagens</div>
               <div className='hover:underline'>Temas</div>
