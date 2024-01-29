051588edb5f784fa2689e851d824998da1411978
Vitoria F <vitoria.s.franca@gmail.com>
2024-01-29 14:27:26 -0300

    CRUD de postagem

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,4 +1,4 @@
-import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+import { GithubLogo,LinkedinLogo } from '@phosphor-icons/react'
 
 function Footer() {
  
@@ -8,12 +8,15 @@ function Footer() {
     <>
         <div className="flex justify-center bg-indigo-900 text-white">
           <div className="container flex flex-col items-center py-4">
-            <p className='text-xl font-bold'>Blog pessoal| Copyright: </p>
-            <p className='text-lg'>Acesse minha redes sociais</p>
+            <p className='text-xl font-bold'>Blog pessoal 💻📖| Copyright:2024</p>
+            <p className='text-lg'>Acesse minha redes sociais: </p>
             <div className='flex gap-2'>
+              <a href="https://www.linkedin.com/in/vit%C3%B3ria-fran%C3%A7a-sousa-a00329256" target="_blank">
               <LinkedinLogo size={48} weight='bold' />
-              <InstagramLogo size={48} weight='bold' />
-              <FacebookLogo size={48} weight='bold' />
+              </a>
+              <a href="https://github.com/VitoriaFrancaS" target='_blank'>
+              <GithubLogo size={48} weight='bold' />
+              </a>
             </div>
           </div>
         </div>
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,4 +1,7 @@
 import homeLogo from '../../assets/home.png'
+import ListaPostagens from '../../components/postagens/listaPostagens/ListaPostagens';
+import ModalPostagem from '../../components/postagens/modalPostagem/NodalPostagem';
+
 import './Home.css';
 
 function Home() {
@@ -11,7 +14,7 @@ function Home() {
               <p className='text-xl'>Expresse aqui seus pensamentos e opiniões</p>
   
               <div className="flex justify-around gap-4">
-              
+                <ModalPostagem/>
                 <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
               </div>
             </div>
@@ -22,6 +25,7 @@ function Home() {
             </div>
           </div>
         </div>
+        <ListaPostagens/>
         
       </>
     );
