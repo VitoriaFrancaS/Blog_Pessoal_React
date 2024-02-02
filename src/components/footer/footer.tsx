56eca28ba08e0f6650a565333c5deb7ec285f0cb
Vitória França Sousa <146133121+VitoriaFrancaS@users.noreply.github.com>
2024-02-02 11:31:09 -0300

    Update footer.tsx

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -16,7 +16,7 @@ function Footer() {
         <div className="flex justify-center bg-indigo-900 text-white">
           <div className="container flex flex-col items-center py-4">
             <p className='text-xl font-bold'>Blog pessoal 💻📖| Copyright:2024</p>
-            <p className='text-lg'>Acesse minha redes sociais: </p>
+            <p className='text-lg'>Acesse minhas redes sociais: </p>
             <div className='flex gap-2'>
               <a href="https://www.linkedin.com/in/vit%C3%B3ria-fran%C3%A7a-sousa-a00329256" target="_blank">
               <LinkedinLogo size={48} weight='bold' />
